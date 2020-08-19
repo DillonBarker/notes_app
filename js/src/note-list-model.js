341215fc59afa162979f8fdd81eafad09a0825f7
DillonBarker <barker.dill98@gmail.com>
2020-08-19 10:43:26 +0100

    refactoring files moving and adding correct spacing

--- js/src/note-list-model.js
+++ js/src/note-list-model.js
@@ -1,15 +1,16 @@
-
 (function(exports) {
-function NoteList(noteClass) {
-  this.list = [];
-  this.noteClass = noteClass;
+
+  function NoteList(noteClass) {
+    this.list = [];
+    this.noteClass = noteClass;
   }
-  NoteList.prototype.listNotes = function() {
-    return this.list;
-  }
+    NoteList.prototype.listNotes = function() {
+      return this.list;
+    }
 
   NoteList.prototype.createNote = function(text) {
     this.list.push(new this.noteClass(text));
   }
+  
   exports.NoteList = NoteList;
 })(this);
--- js/src/note-list-view.js
+++ js/src/note-list-view.js
@@ -1,11 +1,12 @@
 (function(exports) {
-function NoteListView(noteListClass) {
-  this.noteListClass = noteListClass
+
+  function NoteListView(noteListClass) {
+    this.noteListClass = noteListClass
   }
-  NoteListView.prototype.view = function() {
-    array = this.noteListClass.listNotes().map(note => note.getText());
-    return "<ul><li><div>" + array.join("</div></li><li><div>") + "</div></li></ul>";
-  }
+    NoteListView.prototype.view = function() {
+      array = this.noteListClass.listNotes().map(note => note.getText());
+      return "<ul><li><div>" + array.join("</div></li><li><div>") + "</div></li></ul>";
+    }
 
   exports.NoteListView = NoteListView
 })(this);
